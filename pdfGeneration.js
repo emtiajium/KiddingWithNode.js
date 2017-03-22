bbe6f6db3b3a65b8e0cc8d289b52e399897d8896
Emtiaj Hasan <271emtiaj@gmail.com>
2017-03-22 20:43:05 +0600

    Bug found | Callback Hell

--- pdfGeneration.js
+++ pdfGeneration.js
@@ -26,78 +26,79 @@ module.exports.serveForm = function(id) {
 	function queryFromDatabase(callback) {
 		var db = databaseOptions.createConnection;
 		var connection = databaseOptions.connection;
+
 		var sqlStatement = 'SELECT `name`, `dept_name` FROM `academic_info` WHERE `id` = ' + id;
 		db.query(sqlStatement, function(err, rows) {
 			if(err) {
 				console.log(err.message);
-				return;
+				// Handle each error | below
+				callback();
 			}
 			//console.log(rows.length);
 			name = rows[0].name;
 			departmentName = rows[0].dept_name;
-		});
-		
-		sqlStatement = 'SELECT `father_name`, `mother_name`, `nationality` FROM `personal_info` WHERE `id` = ' + id;
-		db.query(sqlStatement, function(err, rows) {
-			fatherName = rows[0].father_name;
-			motherName = rows[0].mother_name;
-			nationality = rows[0].nationality;
-		});
 
-		sqlStatement = 'SELECT `course1`, `course2`, `course3`, `course4`, `course5`, `course6`, `course7`, `course8` FROM `termcourse` WHERE `level_term` =31';
-		db.query(sqlStatement, function(err, rows) {
-			course1 = rows[0].course1;
-			course2 = rows[0].course2;
-			course3 = rows[0].course3;
-			course4 = rows[0].course4;
-			course5 = rows[0].course5;
-			course6 = rows[0].course6;
-			course7 = rows[0].course7;
-			course8 = rows[0].course8;
+      sqlStatement = 'SELECT `father_name`, `mother_name`, `nationality` FROM `personal_info` WHERE `id` = ' + id;
+      db.query(sqlStatement, function(err, rows) {
+        fatherName = rows[0].father_name;
+        motherName = rows[0].mother_name;
+        nationality = rows[0].nationality;
+
+        sqlStatement = 'SELECT `course1`, `course2`, `course3`, `course4`, `course5`, `course6`, `course7`, `course8` FROM `termcourse` WHERE `level_term` =31';
+        db.query(sqlStatement, function(err, rows) {
+          course1 = rows[0].course1;
+          course2 = rows[0].course2;
+          course3 = rows[0].course3;
+          course4 = rows[0].course4;
+          course5 = rows[0].course5;
+          course6 = rows[0].course6;
+          course7 = rows[0].course7;
+          course8 = rows[0].course8;
+
+          sqlStatement = 'SELECT `hall_name` FROM `hall_info` WHERE `id` = ' + id;
+          db.query(sqlStatement, function(err, rows) {
+            hallName = rows[0].hall_name;
+            sqlStatement = 'SELECT SUM(`credit`) AS `total_credit` FROM `level-3term-1`';
+
+            db.query(sqlStatement, function(err, rows) {
+              totalCredit = rows[0].total_credit;
+
+              sqlStatement = 'SELECT * FROM `level-3term-1`';
+              db.query(sqlStatement, function(err, rows) {
+                course1Title = rows[0].course_title;
+                course2Title = rows[1].course_title;
+                course3Title = rows[2].course_title;
+                course4Title = rows[3].course_title;
+                course5Title = rows[4].course_title;
+                course6Title = rows[5].course_title;
+                course7Title = rows[6].course_title;
+                course8Title = rows[7].course_title;
+
+                credit1 = rows[0].credit;
+                credit2 = rows[1].credit;
+                credit3 = rows[2].credit;
+                credit4 = rows[3].credit;
+                credit5 = rows[4].credit;
+                credit6 = rows[5].credit;
+                credit7 = rows[6].credit;
+                credit8 = rows[7].credit;
+
+                sqlStatement = 'SELECT `amount` FROM `termcourse` WHERE `level_term` = 31';
+                db.query(sqlStatement, function(err, rows) {
+                  amount = rows[0].amount;
+
+                  sqlStatement = 'SELECT `donation` FROM `eco_info` WHERE `id` = ' + id;
+                  db.query(sqlStatement, function(err, rows) {
+                    donation = rows[0].donation;
+                    callback();
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
 		});
-
-		sqlStatement = 'SELECT `hall_name` FROM `hall_info` WHERE `id` = ' + id;
-		db.query(sqlStatement, function(err, rows) {
-			hallName = rows[0].hall_name;
-		});
-
-		sqlStatement = 'SELECT SUM(`credit`) AS `total_credit` FROM `level-3term-1`';
-		db.query(sqlStatement, function(err, rows) {
-			totalCredit = rows[0].total_credit;
-		});
-
-		sqlStatement = 'SELECT * FROM `level-3term-1`';
-		db.query(sqlStatement, function(err, rows) {
-			course1Title = rows[0].course_title;
-			course2Title = rows[1].course_title;
-			course3Title = rows[2].course_title;
-			course4Title = rows[3].course_title;
-			course5Title = rows[4].course_title;
-			course6Title = rows[5].course_title;
-			course7Title = rows[6].course_title;
-			course8Title = rows[7].course_title;
-
-			credit1 = rows[0].credit;
-			credit2 = rows[1].credit;
-			credit3 = rows[2].credit;
-			credit4 = rows[3].credit;
-			credit5 = rows[4].credit;
-			credit6 = rows[5].credit;
-			credit7 = rows[6].credit;
-			credit8 = rows[7].credit;
-		});
-
-		sqlStatement = 'SELECT `amount` FROM `termcourse` WHERE `level_term` =31';
-		db.query(sqlStatement, function(err, rows) {
-			amount = rows[0].amount;
-		});
-
-		sqlStatement = 'SELECT `donation` FROM `eco_info` WHERE `id` = ' + id;
-		db.query(sqlStatement, function(err, rows) {
-			donation = rows[0].donation;
-			callback();
-		});
-
 	}
 
 	queryFromDatabase(function logMyNumber() {
